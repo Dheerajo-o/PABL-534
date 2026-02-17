b7995b6621c181dda38ce091c958a1be3364a69e
Dheeraj <hey.dheeraj.bsn@gmail.com>
2026-02-17 20:41:07 +0530

    Update 6th.py

--- Experiment List - 19-01-2026 to 25-01-2026/6th.py
+++ Experiment List - 19-01-2026 to 25-01-2026/6th.py
@@ -3,12 +3,13 @@
 # Input: arr[] = [1, 2, 3, 4, 5]
 # Output: [5, 1, 2, 3, 4]
 # Explanation: If we rotate arr by one position in clockwise 5 come to the front and remaining those are shifted to the end.
-def rotate(arr):
-    n = len(arr)
-    if n == 0:
+class Solution:
+    def rotate(self,arr):
+        n = len(arr)
+        if n == 0:
+            return arr
+        last = arr[-1]  
+        for i in range(n - 1, 0, -1):
+            arr[i] = arr[i - 1]
+        arr[0] = last
         return arr
-    last = arr[-1]  
-    for i in range(n - 1, 0, -1):
-        arr[i] = arr[i - 1]
-    arr[0] = last
-    return arr
