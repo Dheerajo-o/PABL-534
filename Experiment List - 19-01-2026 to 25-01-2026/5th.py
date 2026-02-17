2c2be1b90a3bfb90d3aa86b9304e1d980db10199
Dheeraj <hey.dheeraj.bsn@gmail.com>
2026-02-17 20:40:46 +0530

    Update 5th.py

--- Experiment List - 19-01-2026 to 25-01-2026/5th.py
+++ Experiment List - 19-01-2026 to 25-01-2026/5th.py
@@ -4,11 +4,10 @@
 # Output: 90
 # Explanation: The largest element of the given array is 90.
 # Input: arr[] = [5, 5, 5, 5]
-def largest(arr):
-    max_element = arr[0]
-    
-    for num in arr:
-        if num > max_element:
-            max_element = num
-            
-    return max_element
+class Solution:
+    def largest(self,arr):
+        max_element = arr[0]
+        for num in arr:
+            if num > max_element:
+                max_element = num
+        return max_element
