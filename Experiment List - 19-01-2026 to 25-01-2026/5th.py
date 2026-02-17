f88941854fbcc85c18524f0934e38f551011e940
Dheeraj <hey.dheeraj.bsn@gmail.com>
2026-02-17 20:34:54 +0530

    Update 5th.py

--- Experiment List - 19-01-2026 to 25-01-2026/5th.py
+++ Experiment List - 19-01-2026 to 25-01-2026/5th.py
@@ -1,4 +1,9 @@
 # Given an array arr[]. The task is to find the largest element and return it.
+# Examples:
+# Input: arr[] = [1, 8, 7, 56, 90]
+# Output: 90
+# Explanation: The largest element of the given array is 90.
+# Input: arr[] = [5, 5, 5, 5]
 def largest(arr):
     max_element = arr[0]
     
