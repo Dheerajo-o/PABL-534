0623382448c35d5e7279cb1ab5955fee59772407
Dheeraj <hey.dheeraj.bsn@gmail.com>
2026-02-17 20:36:56 +0530

    Create 7th.py

---
+++ Experiment List - 19-01-2026 to 25-01-2026/7th.py
@@ -0,0 +1,14 @@
+#  You are given an integer array arr[]. You need to find the maximum sum of a 
+# subarray (containing at least one element) in the array arr[]. 
+# Note : A subarray is a continuous part of an array. 
+# Examples: 
+# Input: arr[] = [2, 3, -8, 7, -1, 2, 3] 
+# Output: 11 
+# Explanation: The subarray [7, -1, 2, 3] has the largest sum 11.
+def maxSubArraySum(arr):
+    max_current = arr[0]
+    max_global = arr[0]  
+    for i in range(1, len(arr)):
+        max_current = max(arr[i], max_current + arr[i])
+        max_global = max(max_global, max_current)
+    return max_global
