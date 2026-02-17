cf811be073af1066bcfb7809dbb0ffb101b5c3fb
Dheeraj <hey.dheeraj.bsn@gmail.com>
2026-02-17 20:39:56 +0530

    Update 4th.py

--- Experiment List - 19-01-2026 to 25-01-2026/4th.py
+++ Experiment List - 19-01-2026 to 25-01-2026/4th.py
@@ -10,13 +10,14 @@
 # Input: a[] = [1, 2, 3, 2, 1], b[] = [3, 2, 2, 3, 3, 2]
 # Output: [1, 2, 3]
 # Explanation: Union set of both the arrays will be 1, 2 and 3.
-def findUnion(a, b):
-    union_set = set()
-    
-    for num in a:
-        union_set.add(num)
+class Solution:
+    def findUnion(self,a,b):
+        union_set = set()
         
-    for num in b:
-        union_set.add(num)
-        
-    return list(union_set)
+        for num in a:
+            union_set.add(num)
+            
+        for num in b:
+            union_set.add(num)
+            
+        return list(union_set)
