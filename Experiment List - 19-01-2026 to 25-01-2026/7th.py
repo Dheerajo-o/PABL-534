152d930905ba0673b43350604b393074d6e7a05e
Dheeraj <hey.dheeraj.bsn@gmail.com>
2026-02-17 20:41:40 +0530

    Update 7th.py

--- Experiment List - 19-01-2026 to 25-01-2026/7th.py
+++ Experiment List - 19-01-2026 to 25-01-2026/7th.py
@@ -5,10 +5,11 @@
 # Input: arr[] = [2, 3, -8, 7, -1, 2, 3] 
 # Output: 11 
 # Explanation: The subarray [7, -1, 2, 3] has the largest sum 11.
-def maxSubArraySum(arr):
-    max_current = arr[0]
-    max_global = arr[0]  
-    for i in range(1, len(arr)):
-        max_current = max(arr[i], max_current + arr[i])
-        max_global = max(max_global, max_current)
-    return max_global
+class Solution:
+    def maxSubArraySum(self,arr):
+        max_current = arr[0]
+        max_global = arr[0]  
+        for i in range(1, len(arr)):
+            max_current = max(arr[i], max_current + arr[i])
+            max_global = max(max_global, max_current)
+        return max_global
